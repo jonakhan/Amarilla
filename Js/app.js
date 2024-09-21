d030999bc8c9a5c5882d089503c3ce5a78d3f4ed
jonakhan <145628476+jonakhan@users.noreply.github.com>
2024-09-20 19:45:11 -0500

    Update app.js

--- Js/app.js
+++ Js/app.js
@@ -2,7 +2,7 @@ let Titulo = document.title;
 
 window.addEventListener('blur', () => {
     Titulo = document.title;
-    document.title = "No te vallas, regresa :(";
+    document.title = "Para tí";
 })
 
 window.addEventListener('focus', () => {
